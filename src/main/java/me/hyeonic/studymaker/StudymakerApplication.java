62bfe81656964fa8dfa6efb369f39e4ed81dc41d
hyeonic <evan3566@gmail.com>
2021-02-11 17:26:35 +0900

    feat: init STUDYMAKER
    프로젝트 설정 및 초기 세팅
    application.properties DB 설정 추가

---
+++ src/main/java/me/hyeonic/studymaker/StudymakerApplication.java
@@ -0,0 +1,13 @@
+package me.hyeonic.studymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudymakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hyeonic/studymaker/StudymakerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonic.studymaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
